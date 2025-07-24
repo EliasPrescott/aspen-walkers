93cdf5a95b1d5e9a25660e43900f4f1383452d63
eliasprescott <syzygyprescott@gmail.com>
2025-07-23 22:57:10 -0500

    adding web project

---
+++ web/build-journals.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rm -rf "./public/editions"
+mkdir "./public/editions"
+
+for typ_path in ../editions/*/journal.typ; do
+  edition_path=$(dirname "$typ_path")
+  edition=$(basename "$edition_path")
+  output_folder="./public/editions/$edition"
+  mkdir "$output_folder"
+  typst compile "$typ_path" "$output_folder/journal.pdf"
+  typst compile --ppi 72 --pages 1 "$typ_path" "$output_folder/cover.png"
+  typst compile --ppi 72 --pages '2-5' "$typ_path" "$output_folder/page-{p}.png"
+done
